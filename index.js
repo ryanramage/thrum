da8143dddeb08c48c928865b2489feadfc3a3d65
Ryan Ramage <ryan.ramage@gmail.com>
2020-02-29 15:09:31 -0700

    work on scribbletune patterns and making sure noteOff events are memo'd properly

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const rc = require('rc')
+const { List } = require('immutable')
 exports.midi = require('./lib/midi')
 const lengths = require('./lib/lengths')
-exports.operators = require('./lib/operators')
+exports.ops = require('./lib/operators')
+exports.patterns = require('./lib/patterns')
 exports.setup = (options) => rc('thrum', options)
 
-exports.go = (tick) => {
+exports.tick = (tick) => {
   exports.connect(exports.setup(), {toMidi: exports.toMidi}, tick)
 }
 
@@ -15,9 +17,20 @@ exports.connect = (config, dispatchers, initialState, onClockFunction) => {
   }
 
   let lastState = initialState
+  let futureActions = List([])
+
   let onClock = (spp, outputs) => {
-    let results = onClockFunction({state: lastState, spp})
-    if (results && results.actions) exports.dispatch(dispatchers, results.actions, results.state, {midi: outputs})
+    // clear any future actions
+    futureActions = exports.dispatchMemoActions(spp, futureActions, {midi: outputs})
+
+    let actions = List([])
+    let results = onClockFunction({state: lastState, spp}, actions)
+
+    if (List.isList(results)) {
+      futureActions = futureActions.concat(exports.dispatch(dispatchers, spp, results, {}, {midi: outputs}))
+    } else if (results && results.actions) {
+      futureActions = futureActions.concat(exports.dispatch(dispatchers, spp, results.actions, results.state, {midi: outputs}))
+    }
     if (results && results.state) lastState = results.state // we should clone this
   }
 
@@ -44,19 +57,25 @@ exports.bars = ({state, spp}, baseTimeSignature, structureArray) => {
   return {state, actions: []} // no bars. just default
 }
 
-exports.dispatch = (dispatchers, actions, state, context) => {
-  if (!actions) actions = []
-  actions.forEach(a => {
-    let to = a[0]
-    let msg = a[1]
-    if (!to) return
-    let fn = dispatchers[to]
-    if (!fn) return
-    fn.call(null, msg, state, context)
+// returns a list of memo (future actions)
+exports.dispatch = (dispatchers, spp, actions, state, context) => {
+  return List([]).withMutations(futureActions => {
+    if (!actions) actions = []
+    actions.forEach(a => {
+      let to = a[0]
+      let msg = a[1]
+      if (!to) return
+      let fn = dispatchers[to]
+      if (!fn) return
+      let fa = fn.call(null, spp, msg, state, context)
+      if (fa) futureActions.concat(fa)
+      return true
+    })
+    return futureActions
   })
 }
 
-exports.toMidi = (msg, state, context) => {
+exports.toMidi = (spp, msg, state, context) => {
   let midi = context.midi
   let output = msg.output || Object.keys(midi)[0]
   let channel = msg.channel || 0
@@ -64,11 +83,35 @@ exports.toMidi = (msg, state, context) => {
   let port = midi[output]
   if (!port) return
   let velocity = msg.velocity || 127
-  let length = msg.length || 50
-  port.note(channel, msg.note, velocity, length)
+  let length = msg.length || lengths['8n']
+  if (typeof length === 'string') length = lengths[length]
+  if (length < 1) length = 1
+
+  port.noteOn(channel, msg.note, velocity)
+  let offSpp = spp + length
+  return {spp: offSpp, name: 'midiOff', msg}
+}
+
+exports.midiOff = (spp, msg, context) => {
+  let midi = context.midi
+  let output = msg.output || Object.keys(midi)[0]
+  let channel = msg.channel || 0
+
+  let port = midi[output]
+  if (!port) return
+  let velocity = msg.velocity || 127
+  port.noteOff(channel, msg.note)
 }
 
 exports.onBeat = (spp, name) => {
   let base = lengths[name]
   return (spp % base === 0)
+}
+
+exports.dispatchMemoActions = (spp, futureActions, context) => {
+  return futureActions.filter(action => {
+    if (action.spp !== spp) return true
+    if (action.name === 'midiOff') exports.midiOff(spp, action.msg, context)
+    return false // so filter works
+  })
 }
---
+++ test/patterns.js
@@ -0,0 +1,15 @@
+const test = require('tape')
+const patterns = require('../lib/patterns.js')
+
+test('expandStr', t => {
+  let array = patterns.expandStr('xxx[xx[xx]]')
+  t.deepEquals([ 'x', 'x', 'x', [ 'x', 'x', [ 'x', 'x' ] ] ], array)
+  t.end()
+})
+
+test('recursivelyApplyPatternToNotes', t => {
+  let patternArr = [ 'x', 'x', '-', [ 'x', 'x', [ 'x', 'x' ] ] ]
+  let result = patterns.recursivelyApplyPatternToNotes(patternArr, 24)
+  console.log(result)
+  t.end()
+})
