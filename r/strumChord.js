13557f739bbc66df0fd4ba1a8fdb82a742d330d4
Ryan Ramage <ryan.ramage@gmail.com>
2021-03-08 11:50:56 -0700

    fix 0 spread

--- r/strumChord.js
+++ r/strumChord.js
@@ -4,8 +4,9 @@ const lengths = require('../lib/lengths')
 module.exports = R.curryN(4, play)
 
 function play(chordNotes, options, count, _length, state) {
-  options.spread = 5
-  let length = options.length || _length 
+  let spread = 5
+  if (typeof options.spread === 'number') spread = options.spread
+  let length = options.length || _length
   chordNotes.forEach((n, _i) => {
     if (_i === 0) {
       let _msg = { to: 'toMidi' }
@@ -15,7 +16,7 @@ function play(chordNotes, options, count, _length, state) {
     } else {
       // schedule a note in the future
       let _msg = { to: 'toMidi' }
-      _msg.futureSpp = state.spp + options.spread * _i
+      if (spread > 0) _msg.futureSpp = state.spp + spread * _i
       _msg.note =  n
       _msg.length = length
       state.actions.push(_msg)
