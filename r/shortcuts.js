bad0da3d9a7a57996f3534d8c1c446dab81e4d8e
Ryan Ramage <ryan.ramage@gmail.com>
2025-12-20 21:41:15 -0700

    fix: pass state parameter to pattern function in shortcuts

--- r/shortcuts.js
+++ r/shortcuts.js
@@ -9,50 +9,50 @@ module.exports = (meter) => {
     // Basic drum patterns
     kick: (patternStr) => {
       const defaultPattern = 'x---x---x---x---' // 4 on the floor
-      return pattern(patternStr || defaultPattern, play('C1'))
+      return (state) => pattern(patternStr || defaultPattern, play('C1'), state)
     },
     
     snare: (patternStr) => {
       const defaultPattern = '----x-------x---' // backbeat
-      return pattern(patternStr || defaultPattern, play('D1'))
+      return (state) => pattern(patternStr || defaultPattern, play('D1'), state)
     },
     
     hihat: (patternStr) => {
       const defaultPattern = 'x-x-x-x-x-x-x-x-' // 8th notes
-      return pattern(patternStr || defaultPattern, play('F#1'))
+      return (state) => pattern(patternStr || defaultPattern, play('F#1'), state)
     },
     
     closedHat: (patternStr) => {
       const defaultPattern = 'x-x-x-x-x-x-x-x-'
-      return pattern(patternStr || defaultPattern, play('F#1'))
+      return (state) => pattern(patternStr || defaultPattern, play('F#1'), state)
     },
     
     openHat: (patternStr) => {
       const defaultPattern = '--x---x---x---x-'
-      return pattern(patternStr || defaultPattern, play('A#1'))
+      return (state) => pattern(patternStr || defaultPattern, play('A#1'), state)
     },
     
     clap: (patternStr) => {
       const defaultPattern = '----x-------x---'
-      return pattern(patternStr || defaultPattern, play('D#1'))
+      return (state) => pattern(patternStr || defaultPattern, play('D#1'), state)
     },
     
     rim: (patternStr) => {
       const defaultPattern = '----x-------x---'
-      return pattern(patternStr || defaultPattern, play('C#1'))
+      return (state) => pattern(patternStr || defaultPattern, play('C#1'), state)
     },
     
     // Preset drum kits
     fourOnFloor: () => [
-      pattern('x---x---x---x---', play('C1')),  // kick
-      pattern('----x-------x---', play('D1')),  // snare
-      pattern('x-x-x-x-x-x-x-x-', play('F#1'))  // hihat
+      (state) => pattern('x---x---x---x---', play('C1'), state),  // kick
+      (state) => pattern('----x-------x---', play('D1'), state),  // snare
+      (state) => pattern('x-x-x-x-x-x-x-x-', play('F#1'), state)  // hihat
     ],
     
     breakbeat: () => [
-      pattern('x---x-x---x-x---', play('C1')),  // syncopated kick
-      pattern('----x-------x---', play('D1')),  // snare
-      pattern('x-x-x-x-x-x-x-x-', play('F#1'))  // hihat
+      (state) => pattern('x---x-x---x-x---', play('C1'), state),  // syncopated kick
+      (state) => pattern('----x-------x---', play('D1'), state),  // snare
+      (state) => pattern('x-x-x-x-x-x-x-x-', play('F#1'), state)  // hihat
     ]
   }
 }
