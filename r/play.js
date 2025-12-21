b5b1617e8ea3f2084f25e947e363a3b86def193c
Ryan Ramage <ryan.ramage@gmail.com>
2025-12-20 21:54:13 -0700

    fix: initialize state parameter in play function to prevent undefined error

--- r/play.js
+++ r/play.js
@@ -1,24 +1,21 @@
 const R = require('ramda')
 const lengths = require('../lib/lengths')
 
-module.exports = R.curryN(4, play)
+module.exports = R.curryN(5, play)
 
 function play(options, notes, count, length, state) {
   // Handle different calling patterns
   if (!state) {
-    // Check if we're being called with fewer arguments (from shortcuts)
-    if (typeof options === 'object' && typeof notes === 'string' && typeof count === 'number' && typeof length === 'number' && arguments.length === 4) {
-      // Called as play({}, note, count, length) - missing state, need to return curried function
-      return (st) => play(options, notes, count, length, st)
-    } else if (typeof options === 'string' && typeof notes === 'number' && typeof count === 'number' && typeof length === 'object') {
-      // Called as play(note, count, length, state)
+    // Called as play(note, count, length, state) - options omitted
+    if (typeof options === 'string' && typeof notes === 'number' && typeof count === 'number' && typeof length === 'object') {
       state = length
       length = count
       count = notes
       notes = options
       options = {}
     } else {
-      // Original pattern: we got empty options, shuffle
+      // Called as play({}, note, count, length) - missing state
+      // This shouldn't happen with curryN(5), but keep for safety
       state = length
       length = count
       count = notes
@@ -26,18 +23,20 @@ function play(options, notes, count, length, state) {
       options = {}
     }
   }
+  
   let _msg = { to: 'toMidi' }
-    if (typeof notes === 'object' && notes.octave && typeof notes.octave === 'function') {
-    notes = notes.octave(4) //
+  if (typeof notes === 'object' && notes.octave && typeof notes.octave === 'function') {
+    notes = notes.octave(4)
     let i = count % notes.length
-    _msg.note =  notes[i] // an array
-  } if (typeof notes === 'function') {
+    _msg.note = notes[i]
+  } else if (typeof notes === 'function') {
     let i = count % notes.length
     _msg.note = notes(i)
-  } else if (typeof notes === 'string') _msg.note =  notes
-  else {
+  } else if (typeof notes === 'string') {
+    _msg.note = notes
+  } else {
     let i = count % notes.length
-    _msg.note =  notes[i] // an array
+    _msg.note = notes[i]
   }
   
   // Smart defaults
