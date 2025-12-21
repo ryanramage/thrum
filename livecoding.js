83cd75d84e168d573344e9191956bd6275b42c7c
Ryan Ramage <ryan.ramage@gmail.com>
2025-12-21 13:17:53 -0700

    feat: enhance MIDI logging for better connection and event tracking

--- livecoding.js
+++ livecoding.js
@@ -85,7 +85,13 @@ function midi () {
   JZZ().or('Cannot start MIDI engine!')
   const input = JZZ().openMidiIn(selectedInput)
 
-  console.log('connected livecoding midi clock on', selectedInput)
+  console.log('MIDI', 'Connected to input:', selectedInput)
+  console.log('MIDI', 'Available inputs:', JZZ().info().inputs.map(i => i.name).join(', '))
+  if (Object.keys(outputs).length > 0) {
+    console.log('MIDI', 'Connected outputs:', Object.keys(outputs).map(key => `${key}: ${config.outputs ? config.outputs[key] : config.out}`).join(', '))
+  } else {
+    console.log('MIDI', 'No MIDI outputs configured (midiThru disabled)')
+  }
 
   let spp = 0
   let broadcasting = false
@@ -100,37 +106,62 @@ function midi () {
       // Clock
       case 0xF2:
         let songPosition = (msg[2] << 7) + msg[1] // data2 is msb, data1 is lsb
-        if (broadcasting) broadcasting = false
-        else spp = (songPosition * 6)
+        console.log('MIDI', `Song Position Pointer received: ${songPosition} (SPP: ${songPosition * 6})`)
+        if (broadcasting) {
+          console.log('MIDI', 'Ignoring SPP - currently broadcasting')
+          broadcasting = false
+        } else {
+          spp = (songPosition * 6)
+          console.log('MIDI', `SPP updated to: ${spp}`)
+        }
         break
       case 0xF8:
         spp++
+        // Log every 24 ticks (1 beat) to avoid spam
+        if (spp % 24 === 0) {
+          console.log('MIDI', `Clock tick - SPP: ${spp} (beat: ${Math.floor(spp / 24)})`)
+        }
         break
       case 0xFA:
-        console.log('MIDI', 'Livecoding Start Received')
+        console.log('MIDI', 'Start Received - SPP reset to 0')
+        spp = 0
         break
       case 0xFB:
-        console.log('MIDI', 'Livecoding Continue Received')
+        console.log('MIDI', 'Continue Received')
         break
       case 0xFC:
-        console.log('MIDI', 'Livecoding Stop Received')
+        console.log('MIDI', 'Stop Received')
         break
       default:
+        // Log other MIDI messages for debugging
+        if (msg[0] >= 0xF0) {
+          console.log('MIDI', `System message: 0x${msg[0].toString(16).toUpperCase()}`, msg)
+        }
         break
     }
   })
 
   const server = http.createServer((req, resp) => {
-    console.log('got a request', spp)
     let songPosition = Math.floor(spp / 6)
+    console.log('HTTP', `Reload request received - Current SPP: ${spp}, Song Position: ${songPosition}`)
     broadcasting = true
-    Object.keys(outputs).forEach(key => {
-      if (outputs[key]) outputs[key].send(JZZ.MIDI.songPosition(songPosition))
-    })
+    
+    if (Object.keys(outputs).length > 0) {
+      Object.keys(outputs).forEach(key => {
+        if (outputs[key]) {
+          console.log('MIDI', `Sending SPP ${songPosition} to output: ${key}`)
+          outputs[key].send(JZZ.MIDI.songPosition(songPosition))
+        }
+      })
+    } else {
+      console.log('MIDI', 'No outputs to send SPP to')
+    }
+    
     resp.end('ok')
   })
   server.listen(config.port, err => {
     if (err) console.log(err) && process.exit(1)
-    console.log('livecoding reload server running on port', config.port)
+    console.log('HTTP', `Livecoding reload server running on port ${config.port}`)
+    console.log('HTTP', `Send GET request to http://localhost:${config.port} to trigger reload`)
   })
 }
