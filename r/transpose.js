4471b2ffef1fceb4b606972a91053987a4c0b038
Ryan Ramage <ryan.ramage@gmail.com>
2025-12-20 21:32:24 -0700

    fix: return original action when note property is missing in transpose

--- r/transpose.js
+++ r/transpose.js
@@ -8,8 +8,11 @@ function transpose(interval, exp, _state) {
   let state = expression(exp, _state)
   // transpose all the notes
   state.actions = state.actions.map(a => {
-    if (!a.note) return
-    a.note = Tonal.Note.transpose(a.note, interval)
+    if (!a.note) return a
+    return {
+      ...a,
+      note: Tonal.Note.transpose(a.note, interval)
+    }
   })
   return state
 }
