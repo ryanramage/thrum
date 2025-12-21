acc5546ca166930cf314e9d9fbd66fb88322cd90
Ryan Ramage <ryan.ramage@gmail.com>
2025-12-20 21:44:05 -0700

    fix: wrap play calls in operator functions to fix pattern argument passing

--- r/shortcuts.js
+++ r/shortcuts.js
@@ -9,50 +9,89 @@ module.exports = (meter) => {
     // Basic drum patterns
     kick: (patternStr) => {
       const defaultPattern = 'x---x---x---x---' // 4 on the floor
-      return (state) => pattern(patternStr || defaultPattern, play('C1'), state)
+      return (state) => {
+        const playOp = (count, length, st) => play('C1', count, length, st)
+        return pattern(patternStr || defaultPattern, playOp, state)
+      }
     },
     
     snare: (patternStr) => {
       const defaultPattern = '----x-------x---' // backbeat
-      return (state) => pattern(patternStr || defaultPattern, play('D1'), state)
+      return (state) => {
+        const playOp = (count, length, st) => play('D1', count, length, st)
+        return pattern(patternStr || defaultPattern, playOp, state)
+      }
     },
     
     hihat: (patternStr) => {
       const defaultPattern = 'x-x-x-x-x-x-x-x-' // 8th notes
-      return (state) => pattern(patternStr || defaultPattern, play('F#1'), state)
+      return (state) => {
+        const playOp = (count, length, st) => play('F#1', count, length, st)
+        return pattern(patternStr || defaultPattern, playOp, state)
+      }
     },
     
     closedHat: (patternStr) => {
       const defaultPattern = 'x-x-x-x-x-x-x-x-'
-      return (state) => pattern(patternStr || defaultPattern, play('F#1'), state)
+      return (state) => {
+        const playOp = (count, length, st) => play('F#1', count, length, st)
+        return pattern(patternStr || defaultPattern, playOp, state)
+      }
     },
     
     openHat: (patternStr) => {
       const defaultPattern = '--x---x---x---x-'
-      return (state) => pattern(patternStr || defaultPattern, play('A#1'), state)
+      return (state) => {
+        const playOp = (count, length, st) => play('A#1', count, length, st)
+        return pattern(patternStr || defaultPattern, playOp, state)
+      }
     },
     
     clap: (patternStr) => {
       const defaultPattern = '----x-------x---'
-      return (state) => pattern(patternStr || defaultPattern, play('D#1'), state)
+      return (state) => {
+        const playOp = (count, length, st) => play('D#1', count, length, st)
+        return pattern(patternStr || defaultPattern, playOp, state)
+      }
     },
     
     rim: (patternStr) => {
       const defaultPattern = '----x-------x---'
-      return (state) => pattern(patternStr || defaultPattern, play('C#1'), state)
+      return (state) => {
+        const playOp = (count, length, st) => play('C#1', count, length, st)
+        return pattern(patternStr || defaultPattern, playOp, state)
+      }
     },
     
     // Preset drum kits
     fourOnFloor: () => [
-      (state) => pattern('x---x---x---x---', play('C1'), state),  // kick
-      (state) => pattern('----x-------x---', play('D1'), state),  // snare
-      (state) => pattern('x-x-x-x-x-x-x-x-', play('F#1'), state)  // hihat
+      (state) => {
+        const playOp = (count, length, st) => play('C1', count, length, st)
+        return pattern('x---x---x---x---', playOp, state)
+      },
+      (state) => {
+        const playOp = (count, length, st) => play('D1', count, length, st)
+        return pattern('----x-------x---', playOp, state)
+      },
+      (state) => {
+        const playOp = (count, length, st) => play('F#1', count, length, st)
+        return pattern('x-x-x-x-x-x-x-x-', playOp, state)
+      }
     ],
     
     breakbeat: () => [
-      (state) => pattern('x---x-x---x-x---', play('C1'), state),  // syncopated kick
-      (state) => pattern('----x-------x---', play('D1'), state),  // snare
-      (state) => pattern('x-x-x-x-x-x-x-x-', play('F#1'), state)  // hihat
+      (state) => {
+        const playOp = (count, length, st) => play('C1', count, length, st)
+        return pattern('x---x-x---x-x---', playOp, state)
+      },
+      (state) => {
+        const playOp = (count, length, st) => play('D1', count, length, st)
+        return pattern('----x-------x---', playOp, state)
+      },
+      (state) => {
+        const playOp = (count, length, st) => play('F#1', count, length, st)
+        return pattern('x-x-x-x-x-x-x-x-', playOp, state)
+      }
     ]
   }
 }
